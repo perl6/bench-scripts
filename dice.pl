45e025ba36a1d0ef901763bae0d8c54c2eed0353
GlitchMr <glitchmr@myopera.com>
2012-12-01 21:02:25 +0100

    Fix #1 by conditional roll subroutine.

--- dice.pl
+++ dice.pl
@@ -1,8 +1,12 @@
+my &safe-roll = (try { (1..6).pick(4, :replace) })
+    ?? ({ $^a.pick: $^b, :replace })
+    !! ({ $^a.roll: $^b });
+
 my $chars-to-roll = 600;
 my @count = 0 xx 20;
 
 for ^$chars-to-roll {
-    my @a = (^6).map({ [+] (1..6).pick(4, :replace).sort(-*)[^3] }).sort(-*);
+    my @a = (^6).map({ [+] safe-roll(1..6, 4).sort(-*)[^3] }).sort(-*);
     for @a { @count[$_]++ };
     # say ~@a;
 }
